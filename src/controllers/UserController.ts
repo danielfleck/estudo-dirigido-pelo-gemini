2dcd6767033f7d7b716336d54c211123eb32d898
Daniel Rodrigo Fleck <danielfleck@gmail.com>
2026-01-29 20:50:36 -0300

    feat: implemented data persistence in the controller (update and delete).

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -5,7 +5,9 @@ import { Database } from 'sqlite'
 
 const HTTP_OK: number = 200
 const HTTP_CREATE: number = 201
+const HTTP_NO_CONTENT: number = 204
 const HTTP_BAD_REQUEST: number = 400
+const HTTP_NOT_FOUND: number = 404
 
 interface User {
     id: string;
@@ -45,6 +47,55 @@ class UserController {
         await db.run('INSERT INTO users (id, name, age) VALUES (?, ?, ?)', [newUser.id, newUser.name, newUser.age])
 
         return res.status(HTTP_CREATE).json(newUser)
+
+    }
+
+    async update(req: Request, res: Response) {
+
+        const { id } = req.params
+        const { name, age } = req.body
+
+        if (!name || !age) {
+            return res.status(HTTP_BAD_REQUEST).json({
+                message: "Name and age are required"
+            })
+        }
+
+        const db: Database = await getDatabaseConnection()
+
+        const result = await db.run(
+            'UPDATE users SET name = ?, age = ? WHERE id = ?',
+            [name, age, id]
+        )
+
+        if (result.changes === 0) {
+            return res.status(HTTP_NOT_FOUND).json({
+                error: "User not found"
+            })
+        }
+
+        return res.status(HTTP_OK).json({ id, name, age })
+
+    }
+
+    async delete(req: Request, res: Response) {
+
+        const { id } = req.params
+
+        const db: Database = await getDatabaseConnection()
+
+        const result = await db.run(
+            'DELETE FROM users WHERE id = ?',
+            [id]
+        )
+
+        if (result.changes === 0) {
+            return res.status(HTTP_NOT_FOUND).json({
+                error: "User not found"
+            })
+        }
+
+        return res.status(HTTP_NO_CONTENT).send()
 
     }
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -16,4 +16,8 @@ routes.get("/users", UserController.index)
 
 routes.post("/users", UserController.store)
 
+routes.put('/users/:id', UserController.update)
+
+routes.delete('/users/:id', UserController.delete)
+
 export default routes
