07719a0aa343f3c71f9955781bae4f9129e9a954
Daniel Rodrigo Fleck <danielfleck@gmail.com>
2026-01-31 19:11:51 -0300

    fix: connection ssl

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,7 +2,11 @@ import pg from 'pg'
 
 const { Pool } = pg
 
-// Criamos o pool usando a string de conexão do .env
+// Configuração de SSL para produção
+// Bancos como Render e Neon exigem 'ssl: true' ou com 'rejectUnauthorized: false'
+const isProduction = process.env.NODE_ENV === 'production'
+
 export const db = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: isProduction ? { rejectUnauthorized: false } : undefined
 })
